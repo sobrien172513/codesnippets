76514337b6681e3e14069c3178ddfbb6bf9c9081
Stephen O'Brien <steveobrien73@gmail.com>
2016-12-11 11:48:21 -0500

    Fixes Code Styling
    For Highlightjs module addon

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,7 +5,8 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/highlightjs/styles/agate.css'
       ],
       js: [
         'public/lib/angular/angular.js',
@@ -16,7 +17,10 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/highlightjs/highlight.pack.js',
+        'public/lib/angular-highlightjs/angular-highlightjs.min.js'
+
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
